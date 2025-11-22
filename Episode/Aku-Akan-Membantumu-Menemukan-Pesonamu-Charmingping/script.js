57f9070c9f9704fbd35480eba44c2f58551291cb
Offmon Istrinya twinkleping <169056210+Offmonprst@users.noreply.github.com>
2025-11-22 15:26:13 +0700

    Create script.js

---
+++ Episode/Aku-Akan-Membantumu-Menemukan-Pesonamu-Charmingping/script.js
@@ -0,0 +1,28 @@
+// Variabel
+const menuBtn = document.getElementById("menuBtn");
+const menuBox = document.getElementById("menuBox");
+const whatsapp = document.getElementById("shareWA");
+const copyurl = document.getElementById("Copyurl");
+const facebook =document.getElementById("shareFB");
+const twitter = document.getElementById("shareX");
+const pageUrl = window.location.href;
+
+// Toggle menu
+menuBtn.addEventListener("click", () => {
+    menuBox.classList.toggle("active");
+});
+
+// Share link
+whatsapp.addEventListener("click", () => {
+    window.open(`https://wa.me/?text=${encodeURIComponent(pageUrl)}`);
+});
+copyurl.addEventListener("click", () => {
+    navigator.clipboard.writeText(pageUrl);
+    alert("Link disalin");
+});
+facebook.addEventListener("click", () => {
+    window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(pageUrl)}`, "_blank");
+});
+twitter.addEventListener("click", () => {
+    window.open(`https://twitter.com/intent/tweet?url=${encodeURIComponent(pageUrl)}`, "_blank");
+});
