01e7b80867b21cdf23fb96f7abb4bd45203f6d75
Offmon Istrinya twinkleping <169056210+Offmonprst@users.noreply.github.com>
2025-12-28 06:54:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // === TOGGLE MENU ===
 const menuBtn = document.getElementById("menuBtn");
 const menuBox = document.getElementById("menuBox");
+const loader = document.getElementById("loading-screen");
+const content = document.getElementById("content");
 
 menuBtn.addEventListener("click", () => {
     menuBox.classList.toggle("active");
+});
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        loader.classList.add("hide");
+        setTimeout(() => {
+            loader.style.display = "none";
+            content.style.display = "block";
+        }, 800);
+    }, 3000);
 });
