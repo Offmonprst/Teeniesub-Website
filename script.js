965aff1119883c943299717a157530a2aee04ce4
Offmon Istrinya twinkleping <169056210+Offmonprst@users.noreply.github.com>
2026-02-07 21:24:10 +0700

    update script.js

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ window.addEventListener("scroll", () => {
     searchBox.classList.remove("active");
     headerTitle.classList.remove("hide");
 });
-/*
+
 // ================= LOADING SCREEN =================
 window.addEventListener("load", () => {
     setTimeout(() => {
@@ -117,4 +117,3 @@ window.addEventListener("load", () => {
         }, 800);
     }, 3000);
 });
-*/
